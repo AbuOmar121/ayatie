82992919acbfc8b03280dc6cff4b49c0bd27b8e1
abook <abookyy@gmail.com>
2025-06-17 17:03:44 +0300

    17/6/2025

---
+++ lib/firebase/models/clusters_model.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Clusters{
+  final String cid;
+  final String cname;
+
+  Clusters({
+    required this.cid,
+    required this.cname,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_application_2/screens/home_page.dart';
+import 'firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,8 +14,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blueGrey[50],
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF007915),
+        ),
+      ),
+      home: HomePage(),
     );
   }
 }
